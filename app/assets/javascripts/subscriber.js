72661bae5b1bb10cdfb5e446c69384021d0109cd
Davida Marion <davidamarion@gmail.com>
2014-04-09 08:48:38 -0700

    blog signup is now ajaxed

---
+++ app/assets/javascripts/subscriber.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+      var url = document.querySelector('form.new_subscriber').action;
+      $('.subscriber_submit').click(function (e) {
+          $.ajax({
+              type: 'POST',
+              url: url,
+              data: {
+                  'subscriber[name]': $('#subscriber_name').val(),
+                  'subscriber[email]': $('#subscriber_email').val(),
+                  'authenticity_token': $(".subscriber_token").html().replace(/(\r\n|\n|\r|\s)/gm,"")
+              },
+              success: function (data) {
+                  alert("Thanks, you've been added to the mailing list!");
+                  $('#subscriber_name').val("");
+                  $('#subscriber_email').val("");
+              },
+              error: function(xhr, textStatus, errorThrown) {
+                  alert("There was a problem with your information.");
+                  console.log(data);
+              }
+          });
+          e.preventDefault();
+      });
+  });
