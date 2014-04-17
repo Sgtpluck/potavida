205edc31dfb5b68b082f69dab21e6e1f06a7f98f
Your Name <blake.jennings@gmail.com>
2014-04-17 14:21:12 -0700

    moar deploy assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'capistrano/rvm'
 require 'capistrano/bundler'
-# require 'capistrano/rails/assets'
+require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 
 # config valid only for Capistrano 3.1
