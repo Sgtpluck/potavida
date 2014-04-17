14ab62240d78218e78ecfaf5ce294fdd0693fddb
Your Name <blake.jennings@gmail.com>
2014-04-17 16:36:28 -0700

    changed from localhost for query

--- app/models/data_query.rb
+++ app/models/data_query.rb
@@ -19,6 +19,6 @@ class DataQuery
   end
 
   def get_hist_date(params)
-    HTTParty.get("http://localhost:4000/histo_query?beginning_date=#{CGI.escape params[:beginning_date]}&ending_date=#{CGI.escape params[:ending_date]}")
+    HTTParty.get("http://ec2-54-187-74-129.us-west-2.compute.amazonaws.com/histo_query?beginning_date=#{CGI.escape params[:beginning_date]}&ending_date=#{CGI.escape params[:ending_date]}")
   end
 end
