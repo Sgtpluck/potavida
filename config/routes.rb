a0e1c4aba38cc62ee98bdc7bf38e620688b8b110
Your Name <blake.jennings@gmail.com>
2014-04-21 12:12:55 -0700

    I made a dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ Potaomg::Application.routes.draw do
   get '/signout',               to: 'sessions#destroy', as: :sign_out
   
   get '/profile/:id',           to: 'users#show', as: :user
-  get '/users',                 to: 'users#index', as: :users
-  get '/users/new',             to: 'users#new', as: :new_user
+  get '/users/:id',             to: 'users#index', as: :users
+  get '/users/:id/new',         to: 'users#new', as: :new_user
   post '/users',                to: 'users#create'
   get '/user/:id/edit',         to: 'users#edit', as: :edit_user
   patch '/profile/:id',         to: 'users#update'
